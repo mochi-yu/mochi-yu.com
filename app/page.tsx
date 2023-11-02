c3f5fd6ded1cce1a4ad0633cf960068b179fe69a
mochi-yu <mochi.2225.it@gmail.com>
2023-11-03 01:02:09 +0900

    feat: トップページの名前と画像を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,93 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Box, Divider, Paper, Stack, Typography } from '@mui/material'
+import bgImage from '@/bg-image.jpg'
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
+    <>
+      <Stack>
+        {/* トップページの名前と画像 */}
+        <Box
+          id='top-profile-photo'
+          height='90vh'
+          sx={{
+            backgroundImage: `url(${bgImage.src})`,
+            backgroundSize: '100vw auto',
+            backgroundPositionY: '-150px',
+          }}
+        >
+          <Stack
+            sx={{ bgcolor: 'rgb(255, 255, 255, .5)' }}
+            position='relative'
+            height='100%'
+            alignItems='center'
+            justifyContent='center'
+            display='flex'
+            direction='column'
+            spacing={3}
           >
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <Typography variant='h3'>もっちー / mochi-yu</Typography>
+            <Divider sx={{ width: '20%', borderBottomWidth: 4 }} />
+          </Stack>
+        </Box>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <Box sx={{ height: '500px', boxShadow: '0px -5px 10px gray' }}>
+          {/* 自己紹介の概要 */}
+          <Box></Box>
 
-      <div className={styles.grid}>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          {/* プロダクトの概要 */}
+          <Box></Box>
 
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
+          {/* スキルの概要 */}
+          <Box></Box>
+        </Box>
+      </Stack>
+    </>
   )
 }
